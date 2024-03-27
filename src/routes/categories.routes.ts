c730162a41fcbd81bbf57fc3e5ad5400456ef14d
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-03-26 21:40:58 -0300

    fix(categories): improvements in categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid"
 const categoriesRoutes = Router()
 
 const categories = []
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body
 
   categories.push({ name, description, id: uuidv4() })
