df23b78be79bc6a45fd0f62d04153ff27e5db230
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-04-09 20:44:57 -0300

    feat(category): import category read data promise

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,7 +1,40 @@
+import fs from "fs"
+import { parse as csvParse } from "csv-parse";
+
+import { ICategoryRepository } from "../../repositories/interfaces/ICategoriesRepository";
+
+interface ImportCategory {
+  name: string,
+  description: string
+}
 
 class ImportCategoriesUseCase {
-  execute(file: any) {
-    console.log("execuuuuuuuute", file)
+  constructor(private categoriesRepository: ICategoryRepository) {
+  }
+
+  loadCategories(file: Express.Multer.File): Promise<ImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: ImportCategory[] = []
+
+      const parseFile = csvParse()
+      stream.pipe(parseFile)
+
+      parseFile.on("data", async (line) => {
+        const [name, description] = line;
+        categories.push({
+          name, description
+        })
+      }).on("end", () => {
+        resolve(categories)
+      }).on("error", (err) => {
+        reject(err)
+      })
+    })
+  }
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
 
