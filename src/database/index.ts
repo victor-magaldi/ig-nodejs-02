24801e624e449759a286877802dde8a779e51b6f
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-04-19 19:33:19 -0300

    feat: init connection typeorm

---
+++ src/database/index.ts
@@ -0,0 +1,25 @@
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+  type: "postgres",
+  port: 5432,
+  username: "docker",
+  password: "ignite",
+  database: "rentx",
+  entities: [
+    // Category,
+    // ...
+  ],
+  migrations: [
+    // CreateCategories1616082124654,
+    // ...
+  ],
+});
+
+dataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err)
+  })
