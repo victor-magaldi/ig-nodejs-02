ef210681806a0e8204099a2eb25315d8c3cab251
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-05-04 21:33:13 -0300

    feat: create methods Migration Create Specification

---
+++ src/database/migrations/1714868097352-CreateSpecifications.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+console.log("Lendo SPECIFICATION");
+export class CreateSpecifications1714868097352 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    try {
+      await queryRunner.createTable(
+        new Table({
+          name: "specifications",
+          columns: [
+            {
+              name: "id",
+              type: "uuid",
+              isPrimary: true,
+            },
+            {
+              name: "name",
+              type: "varchar",
+            },
+            {
+              name: "description",
+              type: "varchar",
+            },
+            {
+              name: "created_at",
+              type: "timestamp",
+              default: "now()",
+            },
+          ],
+        }),
+      );
+    } catch (err) {
+      console.log("err", err);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("specifications");
+  }
+}
