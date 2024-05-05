26c45216b4291d74f528e94342092c6d00696cf2
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-05-04 22:34:50 -0300

    feat: create migration createUser

--- src/database/migrations/1714872210109-CreateUsers.ts
+++ src/database/migrations/1714872210109-CreateUsers.ts
@@ -1,24 +1,41 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateCategories1713571344655 implements MigrationInterface {
+export class CreateUsers1714872210109 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     try {
       const table = await queryRunner.createTable(
         new Table({
-          name: "categories",
+          name: "users",
           columns: [
             {
               name: "id",
               type: "uuid",
-              isPrimary: true,
             },
             {
               name: "name",
               type: "varchar",
             },
             {
-              name: "description",
+              name: "userName",
               type: "varchar",
+              isUnique: true,
+            },
+            {
+              name: "password",
+              type: "varchar",
+            },
+            {
+              name: "email",
+              type: "varchar",
+            },
+            {
+              name: "drive_license",
+              type: "varchar",
+            },
+            {
+              name: "is_admin",
+              type: "boolean",
+              default: false,
             },
             {
               name: "created_at",
@@ -28,13 +45,13 @@ export class CreateCategories1713571344655 implements MigrationInterface {
           ],
         }),
       );
-      console.log("Create Table Category", table);
+      console.log("Create Table User", table);
     } catch (err) {
       console.log("ERROR=>", err);
     }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("categories");
+    await queryRunner.dropTable("users");
   }
 }
