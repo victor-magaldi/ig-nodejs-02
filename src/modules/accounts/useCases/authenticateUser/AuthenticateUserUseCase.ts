4ca31210b64ee5c198587ef74d2ce683282fbc81
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-05-06 20:49:23 -0300

    feat: implement authetication useCase and Controller

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,47 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private userRepository: IUsersRepository,
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // user Exists
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+    // password correct
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+    //generate token
+    const token = sign({}, "5729c7e26108b3e23b961bb04b17f45e", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return { user, token };
+  }
+}
+
+export { AuthenticateUserUseCase };
