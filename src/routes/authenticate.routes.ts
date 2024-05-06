ebc45005214808b6f80573b4f2bc0035b768c126
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-05-06 20:49:46 -0300

    feat: implement Auth routes

---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticationRoutes = Router();
+
+const createUserController = new AuthenticateUserController();
+
+authenticationRoutes.post("/sessions", createUserController.handle);
+
+export { authenticationRoutes };
