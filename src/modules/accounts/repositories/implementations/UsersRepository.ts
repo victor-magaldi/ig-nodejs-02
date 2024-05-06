1057d15c9dee753790a2dd7862d802d98897fe3f
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-05-05 21:07:38 -0300

    feat:create repository user

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,26 @@
+import { Repository } from "typeorm";
+
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../IUsersRepository";
+import { User } from "../../entities/User";
+import { dataSource } from "../../../../database";
+
+class UserRepository implements IUsersRepository {
+  private repository: Repository<User>;
+  constructor() {
+    this.repository = dataSource.manager.getRepository(User);
+  }
+  async create({
+    name,
+    username,
+    email,
+    driver_license,
+    password,
+  }: ICreateUsersDTO): Promise<void> {
+    const user = this.repository.create({ name, username, email, driver_license, password });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UserRepository };
