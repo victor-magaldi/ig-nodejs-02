086590d41c23bcc16853563fef2568a9ef6ebcce
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-05-09 20:09:12 -0300

    test: duplicated categories validation

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.test.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.test.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppErros";
 import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
@@ -20,5 +21,16 @@ describe("Create Category", () => {
     const categoryCreated = await createRepositoryInMemory.findByName(category.name);
 
     expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("shoud be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category Description TEst",
+      };
+      await createCategoryUseCase.execute(category);
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
