9bda3a6bec224dfc198ca1423570c7ffd7f38864
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-05-15 20:58:46 -0300

    fix(container): fix error import

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,13 +1,13 @@
 import { container } from "tsyringe";
 
 import { ICategoryRepository } from "../../modules/cars/repositories/interfaces/ICategoriesRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typorm/repositories/CategoriesRepository";
 
 import { ISpecificationsRepository } from "../../modules/cars/repositories/interfaces/ISpecificationsRepository";
-import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typorm/repositories/SpecificationsRepository";
 
 import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
-import { UserRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { UserRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 
 container.registerSingleton<ICategoryRepository>("CategoriesRepository", CategoriesRepository);
 
