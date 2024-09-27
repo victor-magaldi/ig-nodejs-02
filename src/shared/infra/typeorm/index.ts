551dc1de908fa67450e3488af8ed99ba3c8cbd08
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-26 23:22:01 -0300

    chore: create Migration and fix migration fiel

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -7,8 +7,8 @@ const dataSource = new DataSource({
   password: "ignite",
   database: "rentx",
   host: "database_ignite", //localhost
-  entities: ["src/modules/cars/entities/*{.ts,.js}", "src/modules/accounts/entities/*{.ts,.js}"],
-  migrations: ["src/database/migrations/*{.ts,.js}"],
+  entities: ["src/modules/**/entities/*{.ts,.js}", "src/modules/**/entities/*{.ts,.js}"],
+  migrations: ["src/shared/infra/typeorm/migrations/*{.ts,.js}"],
 });
 
 dataSource
